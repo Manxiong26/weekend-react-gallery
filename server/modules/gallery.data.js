425a0aa0df4dfe63596a124d592d17de994c6f88
Maneena Xiong <xiong.maneena@gmail.com>
2021-04-30 22:47:18 -0500

    modified server/modules/data.js to include the new photos i've added on the images.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Artic_wolf.jp', descriptioin: 'Photo of an Artic Wolf.', likes: 0 },
+    { id: 3, path: 'images/blue_whale.jpg', description: 'Phot of a Blue Whale in the Sea.', like: 0 },
+    { id: 4, path: 'images/polarbear_cub.jpg', description: 'Photo of a Polar Bear Cub.', like: 0 }
 ];
 
 module.exports = galleryItems;
